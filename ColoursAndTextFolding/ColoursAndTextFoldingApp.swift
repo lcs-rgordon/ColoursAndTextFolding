2d63540c9e541fd55899f9b9b0eb3213409d968d
Russell Gordon <rgordon@lcs.on.ca>
2021-10-28 16:48:32 -0400

    Initial Commit

---
+++ ColoursAndTextFolding/ColoursAndTextFoldingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColoursAndTextFoldingApp.swift
+//  ColoursAndTextFolding
+//
+//  Created by Russell Gordon on 2021-10-28.
+//
+
+import SwiftUI
+
+@main
+struct ColoursAndTextFoldingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
